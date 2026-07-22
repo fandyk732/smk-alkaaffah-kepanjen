2340e67432c3ebc6a1186509c34b7e1ed8fd98da
Fandyk Aziz Lukmana <fandyk732@guru.smk.belajar.id>
2026-07-22 11:01:33 +0700

    ngerapihin tampilan berita di homepage

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,18 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
+      {
+        protocol: 'https',
+        hostname: '6a56f44fcec0a76b21484386.imgix.net', // Domain CDN gambar lo
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com', // Kalau gambar berita di-upload ke Firebase Storage
+      },
     ],
   },
 };
